5ed21e127b292211c923654fb76e5572951619cc
Xenate <48114132+Xenate@users.noreply.github.com>
2020-05-14 21:55:24 -0400

    Add files via upload

---
+++ main.scala
@@ -0,0 +1,12 @@
+package parser
+
+import scala.io.Source
+import scala.io.StdIn._
+import scala.collection.mutable.ListBuffer
+
+object main {
+    def main(args: Array[String]) = {
+      val parser = new parser()
+      println(parser.filename)
+    }
+  }
---
+++ parser.scala
@@ -0,0 +1,26 @@
+package parser
+
+import scala.io.Source
+import scala.io.StdIn._
+import scala.collection.mutable.ListBuffer
+
+class parser()  {
+  val filename = (readLine + ".nex")
+  var lines = new ListBuffer[String]() // Each line is the file is an element in the ListBuffer
+  def file_content (filename: String): Unit = {
+    var numlines = Source.fromFile(filename).getLines.size
+    while (numlines >= 1) {
+    var content = Source.fromFile(filename).getLines.next()
+    numlines -= 1
+    lines(0) = content
+    // Takes in a file name and adds each line to a ListBuffer
+  }
+}
+  def parser_start (content: String): Unit = {
+    var index = 0
+    while (content.length() > index){
+      content(index)
+      index += 1
+    }
+  }
+}
