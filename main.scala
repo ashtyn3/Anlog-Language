90c30a8070465356376594d49d895933788ef913
Xenate <48114132+Xenate@users.noreply.github.com>
2020-05-15 23:13:23 -0400

    Update main.scala

--- main.scala
+++ main.scala
@@ -1,4 +1,3 @@
-
 import scala.io.Source
 import scala.io.StdIn._
 import scala.collection.mutable.ListBuffer
@@ -6,6 +5,6 @@ import scala.collection.mutable.ListBuffer
 object main {
     def main(args: Array[String]) = {
       val parser = new parser()
-      println(parser.filename)
+
     }
   }
